41baa245afbf6cac6fb2431d734d4b8ff565ff19
Luke749 <48569945+Lucas749@users.noreply.github.com>
2024-06-15 12:51:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='be_datahive',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'requests',
         'pandas',
         'numpy',
-        'io'
     ],
     author='Lucas Schneider',
     author_email='lucas.schneider@cs.ox.ac.uk',
@@ -19,9 +18,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
 )
